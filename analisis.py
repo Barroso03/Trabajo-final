e67f9feb5c34ae8a7992705c8e321033d7116ad4
juaannavarro <91721668+juaannavarro@users.noreply.github.com>
2022-05-12 22:23:48 +0200

    ca

--- analisis.py
+++ analisis.py
@@ -5,21 +5,7 @@ import re #para trabajar con expresiones regulares
 
 #PASO 1: Unimos los archivos de cada una de las carpetas ETFs y Stock
 
-def ruta_carpeta_ETFs():
-    path='/Users/juanlu_navarro/Documents/Carrera Juan/programacion/Trabajo-final/ETFs'
-    os.chdir(path)
-ruta_carpeta_ETFs()
 
-def union_archivos_ETFs(path):
-    archivos= [ x for x in os.listdir() if re.search('.txt',x)]
-    print(archivos)
-    df= pd.DataFrame()
-    for i in archivos:
-        archivo=pd.read_csv(i)
-        df= pd.concat([df,archivo])
-        df.to_csv('datos_archivo_ETFs.csv', sep=';')
-    return df
-union_archivos_ETFs(ruta_carpeta_ETFs())
 
 def ruta_carpeta_Stocks():
     path='/Users/juanlu_navarro/Documents/Carrera Juan/programacion/Trabajo-final/Stocks'
