977ab9ae40abff509e50ebc7e57a0b886a0ffe59
albabernal03 <ALBABERO@MYUAX.COM>
2022-05-12 19:52:21 +0200

    creamos una funcion con la ruta de acceso a la carpeta ETFs

--- analisis.py
+++ analisis.py
@@ -5,3 +5,10 @@ import re #para trabajar con expresiones regulares
 
 #PASO 1: Unimos los archivos de cada una de las carpetas ETFs y Stock
 
+def ruta_carpeta_ETFs():
+    path='/Users/hectorbernaltrujillo/Downloads/archive (3)/ETFs'
+    os.chdir(path)
+ruta_carpeta_ETFs()
+
+
+
