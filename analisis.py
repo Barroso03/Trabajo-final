fbc58448aca5ae0aff84c0ffad1ab22e4bb290c5
albabernal03 <ALBABERO@MYUAX.COM>
2022-05-12 19:49:30 +0200

    importamos las librerias

---
+++ analisis.py
@@ -0,0 +1,7 @@
+# Importamos las librerias necesarias
+import pandas as pd #ºpara trabajar con dataframes
+import os #para trabajar con los directorios
+import re #para trabajar con expresiones regulares
+
+#PASO 1: Unimos los archivos de cada una de las carpetas ETFs y Stock
+
